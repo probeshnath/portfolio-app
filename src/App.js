5b2e9c15d809f2f7e0c9fa35973fd79e5504db3c
probeshnath <probeshn@gmail.com.com>
2022-01-19 14:56:02 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import { useState } from "react";
 import "./App.scss";
 import About from "./components/About";
-import Blogs from "./components/Blogs";
+// import Blogs from "./components/Blogs";
 import Count from "./components/Count";
 import Education from "./components/Education";
 import Home from "./components/Home";
 import Menu from "./components/Menu";
 import Nav from "./components/Nav";
-import ProcessofWork from "./components/ProcessofWork";
+// import ProcessofWork from "./components/ProcessofWork";
 import Services from "./components/Services";
 import Projects from "./components/Projects";
 import Testimonials from "./components/Testimonials";
